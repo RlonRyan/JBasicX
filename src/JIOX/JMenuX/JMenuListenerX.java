a314b5de6c224ed48aa0b6ad536e3411ed36c1b3
RlonRyan <RlonRyan@aol.com>
2013-10-26 19:09:23 -0600

    Menus... Yay!

---
+++ src/JIOX/JMenuX/JMenuListenerX.java
@@ -0,0 +1,21 @@
+/**
+ * @author RlonRyan
+ * @name JSoundX
+ * @version 1.0.0
+ * @date Dec 17, 2011
+ * @info Input-Output interface.
+ *
+ */
+package JIOX.JMenuX;
+
+/**
+ * @author RlonRyan
+ * @name JMenuX
+ */
+public interface JMenuListenerX {
+
+    public void elementSelected(int element);
+
+    public void elementHighlighted(int element);
+
+}
--- src/JIOX/JMenuX/JMenuX.java
+++ src/JIOX/JMenuX/JMenuX.java
@@ -6,12 +6,11 @@
  * @info Input-Output interface.
  *
  */
-package JIOX;
+package JIOX.JMenuX;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,16 +23,20 @@ import java.util.List;
  */
 public class JMenuX {
 
-    private List<String> elements = new ArrayList<String>();
+    public final static int MENU_CHANGED = 0;
+    public final static int ELEMENT_HIGHLIGHTED = 1;
+    public final static int ELEMENT_SELECTED = 2;
+    private List<JMenuListenerX> listeners = new ArrayList<>();
+    private List<String> elements = new ArrayList<>();
     private Point dimensions;
     private Point position;
     private HashMap<String, Color> colorscheme;
     private HashMap<String, Font> fontscheme;
     private int highlighted = 0;
 
-    public void draw(Graphics2D g2d) {
+    synchronized public void draw(Graphics2D g2d) {
         g2d.drawRoundRect(position.x, position.y, dimensions.x, dimensions.y, dimensions.x / 10, dimensions.y / 10);
-        g2d.setColor(new Color(255,255,255,50));
+        g2d.setColor(new Color(255, 255, 255, 50));
         g2d.fillRoundRect(position.x, position.y, dimensions.x, dimensions.y, dimensions.x / 10, dimensions.y / 10);
 
         int x = position.x + dimensions.x / 100;
@@ -42,10 +45,11 @@ public class JMenuX {
         g2d.setColor(colorscheme.get("title"));
         g2d.setFont(fontscheme.get("title"));
         y += g2d.getFontMetrics().getHeight();
+        x += g2d.getFontMetrics().getHeight();
         g2d.drawString(elements.get(0), x, y);
 
-        x += dimensions.x / 100;
-        y += dimensions.y / 100;
+        y += g2d.getFontMetrics().getHeight();
+        x += g2d.getFontMetrics().getHeight();
 
         g2d.setColor(colorscheme.get("body"));
         g2d.setFont(fontscheme.get("body"));
@@ -65,16 +69,17 @@ public class JMenuX {
         }
     }
 
-    public void validate() {
+    synchronized public final void validate() {
         /*
-         * GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+         * GraphicsEnvironment ge =
+         * GraphicsEnvironment.getLocalGraphicsEnvironment();
          * Font[] fonts = ge.getAllFonts();
          * for (Font f : fonts) {
          * System.out.println(f.getFontName());
          * }
          */
         if (this.colorscheme == null) {
-            this.colorscheme = new HashMap<String, Color>();
+            this.colorscheme = new HashMap<>();
         }
         if (!this.colorscheme.containsKey("title") || this.colorscheme.get("title") == null) {
             this.colorscheme.put("title", Color.WHITE);
@@ -86,7 +91,7 @@ public class JMenuX {
             this.colorscheme.put("highlight", Color.YELLOW);
         }
         if (this.fontscheme == null) {
-            this.fontscheme = new HashMap<String, Font>();
+            this.fontscheme = new HashMap<>();
         }
         if (!this.fontscheme.containsKey("title") || this.fontscheme.get("title") == null) {
             this.fontscheme.put("title", new Font("Monospaced", Font.PLAIN, 16));
@@ -97,6 +102,41 @@ public class JMenuX {
         if (!this.fontscheme.containsKey("highlight") || this.fontscheme.get("highlight") == null) {
             this.fontscheme.put("highlight", new Font("Arial", Font.PLAIN, 10));
         }
+    }
+
+    synchronized public final void incrementHighlight() {
+        this.highlighted += 1;
+        if (this.highlighted > elements.size() - 1) {
+            this.highlighted = 0;
+        }
+        fireEvent(ELEMENT_HIGHLIGHTED, highlighted);
+    }
+
+    synchronized public final void deincrementHighlight() {
+        this.highlighted -= 1;
+        if (this.highlighted < 0) {
+            this.highlighted = this.elements.size() - 1;
+        }
+        fireEvent(ELEMENT_HIGHLIGHTED, highlighted);
+    }
+
+    synchronized public final void incrementHighlight(int increment) {
+        this.highlighted += increment;
+        if (this.highlighted > this.elements.size() - 1) {
+            this.highlighted %= (this.elements.size() - 1);
+        }
+        else if (this.highlighted < 0) {
+            this.highlighted = (this.highlighted % this.elements.size()) + this.elements.size() - 1;
+        }
+        fireEvent(ELEMENT_HIGHLIGHTED, highlighted);
+    }
+
+    synchronized public final void selectMenuElement() {
+        fireEvent(ELEMENT_SELECTED, highlighted);
+    }
+
+    synchronized public final void selectMenuElement(int element) {
+        fireEvent(ELEMENT_SELECTED, element);
     }
 
     public JMenuX(int width, int height, int x, int y, String... elements) {
@@ -111,5 +151,32 @@ public class JMenuX {
         this.position = position;
         this.elements.addAll(Arrays.asList(elements));
         this.validate();
+    }
+
+    synchronized public final void addEventListener(JMenuListenerX listener) {
+        listeners.add(listener);
+    }
+
+    synchronized public final void removeEventListener(JMenuListenerX listener) {
+        listeners.remove(listener);
+    }
+
+    synchronized public void fireEvent(int type, int... data) {
+        switch (type) {
+            case MENU_CHANGED:
+                break;
+            case ELEMENT_SELECTED:
+                for (JMenuListenerX e : this.listeners) {
+                    e.elementSelected(data[0]);
+                }
+                break;
+            case ELEMENT_HIGHLIGHTED:
+                for (JMenuListenerX e : this.listeners) {
+                    e.elementHighlighted(data[0]);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
